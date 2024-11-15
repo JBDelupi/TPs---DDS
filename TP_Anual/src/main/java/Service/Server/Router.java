ae6ae78a3590cef4e329c7a4aea83b942fdb1621
Tobias Duren <tduren@frba.utn.edu.ar>
2024-11-14 22:37:54 -0300

    reporte de deccosalud - vista

--- TP_Anual/src/main/java/Service/Server/Router.java
+++ TP_Anual/src/main/java/Service/Server/Router.java
@@ -72,6 +72,7 @@ public class Router {
 
         Server.app().routes(()->{
             get("/deccosalud",((DeccoSaludController)FactoryController.controller("deccosalud"))::index);
+            post("/deccosalud/detalles",((DeccoSaludController)FactoryController.controller("deccosalud"))::mostrarReporte);
         });
 
         Server.app().routes(()->{
