d794808d6e4c3e6e74cd52e240335d34409e9ec9
Juan Bautista Delupí <jdelupi@frba.utn.edu.ar>
2024-12-16 19:01:43 -0300

    imagenes

--- TP_Anual/src/main/java/Models/Repository/Init/scripts4.sql
+++ TP_Anual/src/main/java/Models/Repository/Init/scripts4.sql
@@ -19,8 +19,7 @@ DO $$
 -- Inserting data into persona
 INSERT INTO persona (id, tipo, calle, codigo_de_notificacion, contrasenia, correo_electronico, latitud, localidad, longitud, medioDeNotificacion, nombreUsuario, numero, tipoUsuario)
 VALUES
-    (1, 'Fisico', NULL, 'jdelupi@frba.utn.edu.ar', md5('1'), 'jdelupi@frba.utn.edu.ar', NULL, NULL, NULL, 'Correo', 'jdelupi@frba.utn.edu.ar', NULL, 'FISICO'),
-    (2, 'Juridico', 'Ayacucho ', 'AppleOficial@apple.com', md5('Appleoficial1.'), 'AppleOficial@apple.com', NULL, 'recoleta', NULL, 'Correo', 'Apple', '1184', 'JURIDICO');
+    (1, 'Juridico', 'Ayacucho ', 'AppleOficial@apple.com', md5('Appleoficial1.'), 'AppleOficial@apple.com', NULL, 'recoleta', NULL, 'Correo', 'Apple', '1184', 'JURIDICO');
 
 -- Inserting data into producto
 INSERT INTO producto (id, descripcion, imagen, nombre, rubro)
@@ -30,29 +29,22 @@ VALUES
 -- Inserting data into rol
 INSERT INTO rol (id, id_persona, tipo)
 VALUES
-    (1, 1, 'COLABORADOR'),
-    (2, 2, 'COLABORADOR');
+    (1, 1, 'COLABORADOR');
 
 -- Inserting data into colaborador
 INSERT INTO colaborador (cantidad_viandas_donadas, id, puntaje, tarjeta_codigo)
 VALUES
-    (0, 1, 400000, NULL),
-    (0, 2, 0, NULL);
+    (0, 1, 0, NULL);
 
 -- Inserting data into contribucion
 INSERT INTO contribucion (Id, fecha_de_donacion, id_colaborador, tipo)
 VALUES
-    (1, '2024-12-10', 2, 'ofrecer_producto');
-
--- Inserting data into fisico
-INSERT INTO fisico (fecha_nacimiento, id, nombre, apellido, numero_Documento, tipo_documento)
-VALUES
-    (NULL, 1, 'DelupÍ', 'Juan Bautista', NULL, NULL);
+    (1, '2024-12-10', 1, 'ofrecer_producto');
 
 -- Inserting data into juridico
 INSERT INTO juridico (id, razon_social, tipoJuridico)
 VALUES
-    (2, 'APPLE ARGENTINA S.A', 'EMPRESA');
+    (1, 'APPLE ARGENTINA S.A', 'EMPRESA');
 
 -- Inserting data into ofrecerproducto
 INSERT INTO ofrecerproducto (Id, producto_id, puntos_necesarios, stock)
